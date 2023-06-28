8b048f9135241c17228bab99ab5da27cb180128d
AnirudhGKulkarni <aneegk1234@gmail.com>
2023-06-28 10:12:13 +0530

    10.12 AM

---
+++ Module 2/JS/datatype.js
@@ -0,0 +1,21 @@
+/function/
+ let b =5;
+ function f(){
+     var a = 40;
+     var b= 6;
+
+ }
+ console.log(b);
+
+/nested if .js/
+ var a = 95;
+ if(a>75)
+ {
+     console.log("Destinction");
+ }
+ else if(a>60){
+     console.log("First class");
+ }
+ else if(a>40){
+     console.log("pass");
+ }
---
+++ Module 2/JS/forloop1.js
@@ -0,0 +1,8 @@
+const number = 10;
+let sum =0;
+for (i = 0; i <= number ;i++)
+{
+    sum = sum + i;
+   
+}
+console.log('sum of 10 numbers is :',sum);
