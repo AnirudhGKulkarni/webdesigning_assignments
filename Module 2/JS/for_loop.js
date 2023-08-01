3753bc37f94e01ff7a5a84b59fe57484cc14ab1e
AnirudhGKulkarni <aneegk1234@gmail.com>
2023-08-01 12:34:40 +0530

    save

--- Module 2/JS/for_loop.js
+++
@@ -1,8 +0,0 @@
-const number = 10;
-let sum =0;
-for (i = 0; i <= number ;i++)
-{
-    sum = sum + i;
-   
-}
-console.log('sum of 10 numbers is :',sum);
--- Module 2/JS/function.js
+++
@@ -1,8 +0,0 @@
-/function/
-let b =5;
-function f(){
-    var a = 40;
-    var b= 6;
-
-}
-console.log(b);
